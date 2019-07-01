04c10835712decc87d7df5c8958e49a1a099f046
redbird <cs20131575@u.sogang.ac.kr>
2019-07-01 20:52:00 +0900

    will do facebook timeline

--- testweb/main/views.py
+++ testweb/main/views.py
@@ -1,11 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import time
+
+startTime = time.time()
 
 datas = [
     {
         'author': '코알라',
         'content': '우리는 코알라에서 웹 코딩을 배우고 있습니다.',
-        'publishedAt': '2019-07-01'
+        'publishedAt': startTime
     }
 ]
 
@@ -14,9 +17,12 @@ def main(request):
     if request.method=="POST":
         author = request.POST['author']
         content = request.POST['content']
+        currentTime = time.time()
+
         datas.append({
             'author': author,
             'content': content,
-            'publishedAt': '2019-07-02'
+            'publishedAt': currentTime
         })
+
     return render(request, 'mainfolder/index.html', {'datas': datas})
